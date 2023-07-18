18e6e881cc0e50f73b24f35f61673f9edd4bcd2e
DAKSHTECHIE1 <daksh13579@gmail.com>
2023-07-19 02:46:13 +0530

    Updated the URLS

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ var rightimg=document.getElementsByClassName('right')[0];
 var fileaccess=document.getElementById('file');
 var dropper=document.getElementsByClassName('dropzone')[0];
 var browse=document.getElementById('browse');
+var progresscontainer=document.getElementsByClassName('progress-container')[0];
+var progressfluid=document.getElementsByClassName('progress-fluid')[0];
+var progressbar=document.getElementsByClassName('progress-bar')[0];
+var uploadpercent=document.getElementsByClassName('upload-percent')[0];
+var showshare=document.getElementsByClassName('show-share')[0];
+var copyicon=document.getElementById('copy');
+var input=document.querySelector('.show-link input');
+var senderinput=document.getElementById('sender-input');
+var recieverinput=document.getElementById('reciever-input');
+var emailform=document.getElementById('email-form');
+var sendbtn=document.getElementById('send-btn')
+// const uploadlink="http://localhost:3000/api/files";
+// const sendlink="http://localhost:3000/api/files/send";
+const uploadlink="https://file-sharing-backend-9o0l.onrender.com/api/files";
+const sendlink="https://file-sharing-backend-9o0l.onrender.com/api/files/send";
 dropper.addEventListener('dragover',(e)=>{
     e.preventDefault();
     console.log('aaa');
@@ -35,13 +50,106 @@ dropper.addEventListener('drop',(e)=>{
     console.log(files);
     if(files.length!=0){
         fileaccess.files=files;
+        uploadFile();
     }
 })
 browse.addEventListener('click',()=>{
     fileaccess.click();
 })
+fileaccess.addEventListener('change',()=>{
+    uploadFile();
+})
+copyicon.addEventListener('click',()=>{
+    input.select();
+    document.execCommand('copy');
+})
+
+
+
+
+
+
 
 const uploadFile=()=>{
-    const dile=fileInput.Files[0];
-    const formData= new FormData;
+    console.log('uploading')
+    progresscontainer.style.display='block';
+    const file=fileaccess.files[0];
+    const formData= new FormData();
+    formData.append('myfile',file);
+    const xhrRequest=new  XMLHttpRequest();
+    //called when req is completed successfully
+    xhrRequest.onload=()=>{
+        console.log(xhrRequest.response);
+        showlink(JSON.parse(xhrRequest.response));
+    };
+    xhrRequest.upload.onprogress=updateprogress;
+    xhrRequest.open('POST',uploadlink);
+    xhrRequest.send(formData);
+}
+
+
+
+
+
+const updateprogress=(e)=>{
+    const percentloaded=Math.round((e.loaded/e.total)*100);
+    console.log(e);
+    console.log(percentloaded);
+    progressfluid.style.width=`${percentloaded}%`;
+    progressbar.style.transform=`scale(${percentloaded/100})`;
+    uploadpercent.innerHTML=`${percentloaded}%`;
+}
+
+
+
+
+
+
+var a=0;
+const showlink=({file})=>{
+  
+   progresscontainer.style.display='none'; 
+   showshare.style.display='block';
+   input.value=file;
+   console.log('file',file);
+   const sendfunc=()=>{
+        console.log('senenenene');
+        const sender=senderinput.value;
+        const reciever=recieverinput.value;
+        const uuid=file.split('/')[4]
+        const data={
+            uuid,
+            reciever,
+            sender
+        };
+        fetch(sendlink,{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(data)
+        }).then((res)=>{
+            return res.json()
+        })
+        .then((info)=>{
+            console.log(info)//{success: true}
+            if(info.success){
+                showshare.style.display='none';
+            }
+            sendbtn.removeEventListener('click',sendfunc);
+            a--;
+        })
+   }
+   console.log('a',a)
+   if(a==1){
+        console.log('inn')
+        var new_element = sendbtn.cloneNode(true);
+        sendbtn.parentNode.replaceChild(new_element, sendbtn);
+        sendbtn=new_element;
+        a--; 
+   }
+   console.log('a',a)
+   sendbtn.addEventListener('click',sendfunc);
+   a++;
+   console.log('a',a);
 }
