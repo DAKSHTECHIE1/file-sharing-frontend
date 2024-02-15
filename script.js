5fc6f81981d890bad988b3684f6eeb3349fab86f
DAKSHTECHIE1 <daksh13579@gmail.com>
2024-02-16 00:43:19 +0530

    bugs-fixed-2

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ var senderinput=document.getElementById('sender-input');
 var recieverinput=document.getElementById('reciever-input');
 var emailform=document.getElementById('email-form');
 var sendbtn=document.getElementById('send-btn')
-const uploadlink="http://localhost:3000/api/files";
-const sendlink="http://localhost:3000/api/files/send";
-// const uploadlink="https://file-sharing-backend-9o0l.onrender.com/api/files";
-// const sendlink="https://file-sharing-backend-9o0l.onrender.com/api/files/send";
+// const uploadlink="http://localhost:3000/api/files";
+// const sendlink="http://localhost:3000/api/files/send";
+const uploadlink="https://file-sharing-backend-9o0l.onrender.com/api/files";
+const sendlink="https://file-sharing-backend-9o0l.onrender.com/api/files/send";
 dropper.addEventListener('dragover',(e)=>{
     e.preventDefault();
     console.log('aaa');
